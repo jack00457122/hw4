044052929c9c2a1a186504494971188377cd5b3a
gitHub <cyc00201@gmail.com>
2020-05-18 15:25:13 +0800

    Initial Commit

---
+++ hw4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw4
+//
+//  Created by User06 on 2020/5/18.
+//  Copyright © 2020 bc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
